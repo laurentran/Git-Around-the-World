04f3833c3c023688d605dc0aa6f7ddad886807ae
Adina Shanholtz <ashanhol@gmail.com>
2016-04-14 14:00:41 -0400

    Added web aspect
    Added web visualization that used http://datamaps.github.io/.

---
+++ mydata.js
@@ -0,0 +1,83 @@
+var data = [
+       {
+        origin: {
+            latitude: 20.639722,
+            longitude: 79.778889
+        },
+        destination: {
+            latitude: 37.618889,
+            longitude: -100.375
+        },
+         options: {
+          strokeWidth: 6,
+         }
+      },
+      {
+        origin: {
+            latitude: 20.639722,
+            longitude: 79.778889
+        },
+        destination: {
+            latitude: 55.618889,
+            longitude: -100.375
+        },
+         options: {
+          strokeWidth: 3,
+         }
+      },
+      {
+          origin: {
+            latitude: 37.618889,
+            longitude: -100.375
+          },
+          destination: {
+              latitude: 56.793333,
+              longitude: 50.290556
+          },
+          options: {
+          strokeWidth: 1,
+         }
+      },
+        
+      {
+          origin: {
+            latitude: -15.618889,
+            longitude: -50.375
+          },
+          destination: {
+              latitude: -30.793333,
+              longitude: 30.290556
+          },
+          options: {
+          strokeWidth: 1,
+         }
+      },
+       {
+          origin: {
+            latitude: 37.618889,
+            longitude: -100.375
+          },
+          destination: {
+              latitude: -30.793333,
+              longitude: 30.290556
+          },
+          options: {
+          strokeWidth: 2,
+         }
+      },
+        
+        {
+         origin: {
+              latitude: 25.194444,
+              longitude: -100.67
+          },
+          destination: {
+              latitude: 25.793333,
+              longitude: -0.290556
+          },
+         options: {
+          strokeWidth: 2,
+         }
+       }
+        
+      ];
